a7a798fd06bdd5ce74d4b8d4165ac98c2922a098
Manish Malhotra <manish.malhotra@appstreet.io>
2016-12-08 16:22:58 +0530

    Update the Activity

--- tv/src/main/java/com/example/iappstreet/exoplayerdemo/ExoPlayerActivity.java
+++ tv/src/main/java/com/example/iappstreet/exoplayerdemo/ExoPlayerActivity.java
@@ -4,12 +4,19 @@ import android.os.Bundle;
 import android.app.Activity;
 
 
-public class MainActivity extends Activity {
+public class ExoPlayerActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+
+
+
+
     }
 
 }
