2bc0f9938d6991122c668e8e9524ff81c9b7f537
Manish Malhotra <manish.malhotra@appstreet.io>
2016-12-14 12:47:07 +0530

    Add the textureview and simpleexoplayer

--- tv/src/main/java/com/example/iappstreet/exoplayerdemo/ExoPlayerActivity.java
+++ tv/src/main/java/com/example/iappstreet/exoplayerdemo/ExoPlayerActivity.java
@@ -1,22 +1,55 @@
 package com.example.iappstreet.exoplayerdemo;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.graphics.SurfaceTexture;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.TextureView;
+
+import com.google.android.exoplayer2.DefaultLoadControl;
+import com.google.android.exoplayer2.ExoPlayer;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.ExoPlayerLibraryInfo;
+import com.google.android.exoplayer2.Renderer;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
 
 
-public class ExoPlayerActivity extends Activity {
+public class ExoPlayerActivity extends Activity implements TextureView.SurfaceTextureListener {
+
+    private TextureView mTextureView;
+    SimpleExoPlayer player;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-
-
-
+        mTextureView = (TextureView) findViewById(R.id.videoview);
+        player = ExoPlayerFactory.newSimpleInstance(ExoPlayerActivity.this, new DefaultTrackSelector(new Handler()), new DefaultLoadControl());
+        player.setPlayWhenReady(true);
+        player.setVideoTextureView(mTextureView);
 
 
     }
 
+    @Override
+    public void onSurfaceTextureAvailable(SurfaceTexture surfaceTexture, int i, int i1) {
+
+    }
+
+    @Override
+    public void onSurfaceTextureSizeChanged(SurfaceTexture surfaceTexture, int i, int i1) {
+
+    }
+
+    @Override
+    public boolean onSurfaceTextureDestroyed(SurfaceTexture surfaceTexture) {
+        return false;
+    }
+
+    @Override
+    public void onSurfaceTextureUpdated(SurfaceTexture surfaceTexture) {
+
+    }
 }
