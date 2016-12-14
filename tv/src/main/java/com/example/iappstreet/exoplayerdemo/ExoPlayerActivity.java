c18a52fe8f3a4848095efabadc450dae639895b3
Manish Verma <manish.verma@iappstreet.com>
2016-12-14 16:53:09 +0530

    play content with texture view hardcore url for  eros now movie

--- tv/src/main/java/com/example/iappstreet/exoplayerdemo/ExoPlayerActivity.java
+++ tv/src/main/java/com/example/iappstreet/exoplayerdemo/ExoPlayerActivity.java
@@ -1,24 +1,164 @@
 package com.example.iappstreet.exoplayerdemo;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.graphics.SurfaceTexture;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
+import android.text.TextUtils;
 import android.view.TextureView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.iappstreet.exoplayerdemo.Utils.EventLogger;
+import com.example.iappstreet.exoplayerdemo.Utils.MyApplication;
+import com.example.iappstreet.exoplayerdemo.model.Playlist;
+import com.example.iappstreet.exoplayerdemo.model.PlaylistItem;
+import com.google.android.exoplayer2.C;
 import com.google.android.exoplayer2.DefaultLoadControl;
+import com.google.android.exoplayer2.ExoPlaybackException;
 import com.google.android.exoplayer2.ExoPlayer;
 import com.google.android.exoplayer2.ExoPlayerFactory;
 import com.google.android.exoplayer2.ExoPlayerLibraryInfo;
 import com.google.android.exoplayer2.Renderer;
 import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.Timeline;
+import com.google.android.exoplayer2.drm.DrmSessionManager;
+import com.google.android.exoplayer2.drm.FrameworkMediaCrypto;
+import com.google.android.exoplayer2.drm.FrameworkMediaDrm;
+import com.google.android.exoplayer2.drm.HttpMediaDrmCallback;
+import com.google.android.exoplayer2.drm.StreamingDrmSessionManager;
+import com.google.android.exoplayer2.drm.UnsupportedDrmException;
+import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
+import com.google.android.exoplayer2.source.ConcatenatingMediaSource;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.source.dash.DashMediaSource;
+import com.google.android.exoplayer2.source.dash.DefaultDashChunkSource;
+import com.google.android.exoplayer2.source.hls.HlsMediaSource;
+import com.google.android.exoplayer2.source.smoothstreaming.DefaultSsChunkSource;
+import com.google.android.exoplayer2.source.smoothstreaming.SsMediaSource;
+import com.google.android.exoplayer2.trackselection.AdaptiveVideoTrackSelection;
 import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.MappingTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelection;
+import com.google.android.exoplayer2.trackselection.TrackSelections;
+import com.google.android.exoplayer2.trackselection.TrackSelector;
+import com.google.android.exoplayer2.ui.DebugTextViewHelper;
+import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
+import com.google.android.exoplayer2.upstream.DataSource;
+import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
+import com.google.android.exoplayer2.upstream.HttpDataSource;
+import com.google.android.exoplayer2.util.Util;
+
+import java.net.CookieHandler;
+import java.net.CookieManager;
+import java.net.CookiePolicy;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+import static com.example.iappstreet.exoplayerdemo.PlayerActivity.PREFER_EXTENSION_DECODERS;
 
 
-public class ExoPlayerActivity extends Activity implements TextureView.SurfaceTextureListener {
+public class ExoPlayerActivity extends Activity implements TextureView.SurfaceTextureListener,ExoPlayer.EventListener,
+        TrackSelector.EventListener<MappingTrackSelector.MappedTrackInfo> {
 
     private TextureView mTextureView;
     SimpleExoPlayer player;
+
+    private String mCurrentPath = "http://movieshls001-a.erosnow.com/hls/movie/4/1023354/theatricaltrailer/6613129/1023354_6613129_IPAD_ALL_multi.m3u8";
+
+    public static final String DRM_SCHEME_UUID_EXTRA = "drm_scheme_uuid";
+    public static final String DRM_LICENSE_URL = "drm_license_url";
+    public static final String DRM_KEY_REQUEST_PROPERTIES = "drm_key_request_properties";
+    public static final String PREFER_EXTENSION_DECODERS = "prefer_extension_decoders";
+    private boolean mIsStreaming = false;
+    private  PLAYBACK_ASSET mPlaybackAsset;
+    private  AD_TYPE mAdType;
+    public static final String ACTION_VIEW = "com.google.android.exoplayer.demo.action.VIEW";
+    public static final String EXTENSION_EXTRA = "extension";
+    private PlaylistItem mPlaylistItem;
+
+    public static final String ACTION_VIEW_LIST =
+            "com.google.android.exoplayer.demo.action.VIEW_LIST";
+    public static final String URI_LIST_EXTRA = "uri_list";
+    public static final String EXTENSION_LIST_EXTRA = "extension_list";
+
+    public static String KEY_PLAYLIST = "KEY_PLAYLIST";
+
+    private static final DefaultBandwidthMeter BANDWIDTH_METER = new DefaultBandwidthMeter();
+    private static final CookieManager DEFAULT_COOKIE_MANAGER;
+
+    static {
+        DEFAULT_COOKIE_MANAGER = new CookieManager();
+        DEFAULT_COOKIE_MANAGER.setCookiePolicy(CookiePolicy.ACCEPT_ORIGINAL_SERVER);
+    }
+
+    @Override
+    public void onLoadingChanged(boolean b) {
+
+    }
+
+    @Override
+    public void onPlayerStateChanged(boolean b, int i) {
+
+    }
+
+    @Override
+    public void onTimelineChanged(Timeline timeline, Object o) {
+
+    }
+
+    @Override
+    public void onPlayerError(ExoPlaybackException e) {
+
+    }
+
+    @Override
+    public void onPositionDiscontinuity() {
+
+    }
+
+    @Override
+    public void onTrackSelectionsChanged(TrackSelections<? extends MappingTrackSelector.MappedTrackInfo> trackSelections) {
+
+    }
+
+    private enum PLAYBACK_ASSET {
+        AD,
+        CONTENT
+    }
+
+    private enum AD_TYPE {
+        PREROLL,
+        MIDROLL,
+        POSTROLL
+    }
+
+    private Handler mainHandler;
+    private Timeline.Window window;
+    private EventLogger eventLogger;
+//    private SimpleExoPlayerView simpleExoPlayerView;
+    private LinearLayout debugRootView;
+    private TextView debugTextView;
+    private Button retryButton;
+    public static String KEY_BUNDLE_EXTRA = "KEY_BUNDLE_EXTRA";
+
+    private DataSource.Factory mediaDataSourceFactory;
+    private MappingTrackSelector trackSelector;
+    //  private TrackSelectionHelper trackSelectionHelper;
+    private DebugTextViewHelper debugViewHelper;
+    private boolean playerNeedsSource;
+
+    private boolean shouldAutoPlay;
+    private boolean isTimelineStatic;
+    private int playerWindow;
+    private long playerPosition;
 
 
     @Override
@@ -26,11 +166,245 @@ public class ExoPlayerActivity extends Activity implements TextureView.SurfaceTe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mTextureView = (TextureView) findViewById(R.id.videoview);
-        player = ExoPlayerFactory.newSimpleInstance(ExoPlayerActivity.this, new DefaultTrackSelector(new Handler()), new DefaultLoadControl());
-        player.setPlayWhenReady(true);
-        player.setVideoTextureView(mTextureView);
+        //player = ExoPlayerFactory.newSimpleInstance(ExoPlayerActivity.this, new DefaultTrackSelector(new Handler()), new DefaultLoadControl());
 
 
+        shouldAutoPlay = true;
+        mediaDataSourceFactory = buildDataSourceFactory(true);
+        mainHandler = new Handler();
+        window = new Timeline.Window();
+        if (CookieHandler.getDefault() != DEFAULT_COOKIE_MANAGER) {
+            CookieHandler.setDefault(DEFAULT_COOKIE_MANAGER);
+        }
+//        mVideoPlazaPlugs = VideoplazaPlugin.getInstance();
+
+//        simpleExoPlayerView = (SimpleExoPlayerView) findViewById(R.id.player_view);
+//        simpleExoPlayerView.setControllerVisibilityListener(this);
+//        simpleExoPlayerView.requestFocus();
+
+
+
+
+    }
+
+
+    private DataSource.Factory buildDataSourceFactory(boolean useBandwidthMeter) {
+        return ((MyApplication) getApplication())
+                .buildDataSourceFactory(useBandwidthMeter ? BANDWIDTH_METER : null);
+    }
+
+    @Override
+    public void onNewIntent(Intent intent) {
+        releasePlayer();
+        isTimelineStatic = false;
+        setIntent(intent);
+    }
+
+
+    private void releasePlayer() {
+        if (player != null) {
+//            debugViewHelper.stop();
+            debugViewHelper = null;
+            shouldAutoPlay = player.getPlayWhenReady();
+            playerWindow = player.getCurrentWindowIndex();
+            playerPosition = C.TIME_UNSET;
+            Timeline timeline = player.getCurrentTimeline();
+            if (timeline != null && timeline.getWindow(playerWindow, window).isSeekable) {
+                playerPosition = player.getCurrentPosition();
+            }
+            player.release();
+            player = null;
+            trackSelector = null;
+            //    trackSelectionHelper = null;
+            eventLogger = null;
+        }
+    }
+
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        if (Util.SDK_INT > 23) {
+            initializePlayer();
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if ((Util.SDK_INT <= 23 || player == null)) {
+            initializePlayer();
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (Util.SDK_INT <= 23) {
+            releasePlayer();
+        }
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (Util.SDK_INT > 23) {
+            releasePlayer();
+        }
+    }
+
+
+    private void initializePlayer() {
+        Intent intent = getIntent();
+        if (player == null) {
+            boolean preferExtensionDecoders = intent.getBooleanExtra(PREFER_EXTENSION_DECODERS, false);
+            UUID drmSchemeUuid = intent.hasExtra(DRM_SCHEME_UUID_EXTRA)
+                    ? UUID.fromString(intent.getStringExtra(DRM_SCHEME_UUID_EXTRA)) : null;
+            DrmSessionManager<FrameworkMediaCrypto> drmSessionManager = null;
+            if (drmSchemeUuid != null) {
+                String drmLicenseUrl = intent.getStringExtra(DRM_LICENSE_URL);
+                String[] keyRequestPropertiesArray = intent.getStringArrayExtra(DRM_KEY_REQUEST_PROPERTIES);
+                Map<String, String> keyRequestProperties;
+                if (keyRequestPropertiesArray == null || keyRequestPropertiesArray.length < 2) {
+                    keyRequestProperties = null;
+                } else {
+                    keyRequestProperties = new HashMap<>();
+                    for (int i = 0; i < keyRequestPropertiesArray.length - 1; i += 2) {
+                        keyRequestProperties.put(keyRequestPropertiesArray[i],
+                                keyRequestPropertiesArray[i + 1]);
+                    }
+                }
+                try {
+                    drmSessionManager = buildDrmSessionManager(drmSchemeUuid, drmLicenseUrl,
+                            keyRequestProperties);
+                } catch (UnsupportedDrmException e) {
+                    int errorStringId = Util.SDK_INT < 18 ? R.string.error_drm_not_supported
+                            : (e.reason == UnsupportedDrmException.REASON_UNSUPPORTED_SCHEME
+                            ? R.string.error_drm_unsupported_scheme : R.string.error_drm_unknown);
+                    showToast(errorStringId);
+                    return;
+                }
+            }
+
+            eventLogger = new EventLogger();
+            TrackSelection.Factory videoTrackSelectionFactory =
+                    new AdaptiveVideoTrackSelection.Factory(BANDWIDTH_METER);
+            trackSelector = new DefaultTrackSelector(mainHandler, videoTrackSelectionFactory);
+            trackSelector.addListener(this);
+            trackSelector.addListener(eventLogger);
+            // trackSelectionHelper = new TrackSelectionHelper(trackSelector, videoTrackSelectionFactory);*/
+            player = ExoPlayerFactory.newSimpleInstance(this, trackSelector, new DefaultLoadControl(),
+                    drmSessionManager, preferExtensionDecoders);
+            player.addListener(this);
+            player.addListener(eventLogger);
+            player.setAudioDebugListener(eventLogger);
+            player.setVideoDebugListener(eventLogger);
+            player.setId3Output(eventLogger);
+            player.setPlayWhenReady(true);
+            player.setVideoTextureView(mTextureView);
+//            simpleExoPlayerView.setPlayer(player);
+            if (isTimelineStatic) {
+                if (playerPosition == C.TIME_UNSET) {
+                    player.seekToDefaultPosition(playerWindow);
+                } else {
+                    player.seekTo(playerWindow, playerPosition);
+                }
+            }
+            player.setPlayWhenReady(shouldAutoPlay);
+//            debugViewHelper = new DebugTextViewHelper(player, debugTextView);
+//            debugViewHelper.start();
+            playerNeedsSource = true;
+        }
+
+
+            startPlay();
+
+    }
+
+
+
+
+    private void startPlay() {
+
+        if (playerNeedsSource) {
+
+            Uri uris = Uri.parse(mCurrentPath);
+            String[] extensions;
+     /* if (ACTION_VIEW.equals(action)) {
+        uris = new Uri[] {intent.getData()};
+        extensions = new String[] {intent.getStringExtra(EXTENSION_EXTRA)};
+      }*/ /*else if (ACTION_VIEW_LIST.equals(action)) {
+        String[] uriStrings = intent.getStringArrayExtra(URI_LIST_EXTRA);
+        uris = new Uri[uriStrings.length];
+        for (int i = 0; i < uriStrings.length; i++) {
+          uris[i] = Uri.parse(uriStrings[i]);
+        }
+        extensions = intent.getStringArrayExtra(EXTENSION_LIST_EXTRA);
+        if (extensions == null) {
+          extensions = new String[uriStrings.length];
+        }
+      } else {
+        showToast(getString(R.string.unexpected_intent_action, action));
+        return;
+      }*/
+            if (Util.maybeRequestReadExternalStoragePermission(this, uris)) {
+                // The player will be reinitialized if the permission is granted.
+                return;
+            }
+            MediaSource[] mediaSources = new MediaSource[1];
+            mediaSources[0] = buildMediaSource(uris, null);
+
+            MediaSource mediaSource = mediaSources.length == 1 ? mediaSources[0]
+                    : new ConcatenatingMediaSource(mediaSources);
+            player.prepare(mediaSource, !isTimelineStatic, !isTimelineStatic);
+            playerNeedsSource = false;
+//            updateButtonVisibilities();
+        }
+    }
+
+    private MediaSource buildMediaSource(Uri uri, String overrideExtension) {
+        int type = Util.inferContentType(!TextUtils.isEmpty(overrideExtension) ? "." + overrideExtension
+                : uri.getLastPathSegment());
+        switch (type) {
+            case C.TYPE_SS:
+                return new SsMediaSource(uri, buildDataSourceFactory(false),
+                        new DefaultSsChunkSource.Factory(mediaDataSourceFactory), mainHandler, eventLogger);
+            case C.TYPE_DASH:
+                return new DashMediaSource(uri, buildDataSourceFactory(false),
+                        new DefaultDashChunkSource.Factory(mediaDataSourceFactory), mainHandler, eventLogger);
+            case C.TYPE_HLS:
+                return new HlsMediaSource(uri, mediaDataSourceFactory, mainHandler, eventLogger);
+            case C.TYPE_OTHER:
+                return new ExtractorMediaSource(uri, mediaDataSourceFactory, new DefaultExtractorsFactory(),
+                        mainHandler, eventLogger);
+            default: {
+                throw new IllegalStateException("Unsupported type: " + type);
+            }
+        }
+    }
+
+    private DrmSessionManager<FrameworkMediaCrypto> buildDrmSessionManager(UUID uuid,
+                                                                           String licenseUrl, Map<String, String> keyRequestProperties) throws UnsupportedDrmException {
+        if (Util.SDK_INT < 18) {
+            return null;
+        }
+        HttpMediaDrmCallback drmCallback = new HttpMediaDrmCallback(licenseUrl,
+                buildHttpDataSourceFactory(false), keyRequestProperties);
+        return new StreamingDrmSessionManager<>(uuid,
+                FrameworkMediaDrm.newInstance(uuid), drmCallback, null, mainHandler, eventLogger);
+    }
+
+    private HttpDataSource.Factory buildHttpDataSourceFactory(boolean useBandwidthMeter) {
+        return ((MyApplication) getApplicationContext())
+                .buildHttpDataSourceFactory(useBandwidthMeter ? BANDWIDTH_METER : null);
+    }
+
+    private void showToast(int messageId) {
+        showToast(getString(messageId));
+    }
+
+    private void showToast(String message) {
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
     }
 
     @Override
